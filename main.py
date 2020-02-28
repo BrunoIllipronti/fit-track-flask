d03796030175dbcb97f7fdb19af31a8722b45d5b
Bruno <bruno_cpp@hotmail.com>
2020-02-28 01:38:57 -0600

    Chart JS exploration

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import psycopg2
+import json
 
 app = Flask(__name__)
+test = 'hey'
 
 @app.route('/')
 def home():
@@ -23,18 +25,33 @@ def login():
 
             cursor = conn.cursor()
 
-            query = 'SELECT person_id, first_name, last_name FROM public."Person" WHERE username = ' + "'" + user + "'" + ' AND pwd = ' + "'" + pwd + "'"           
+            query = 'SELECT person_id, first_name, last_name, workout_location FROM public."Person" WHERE username = ' + "'" + user + "'" + ' AND pwd = ' + "'" + pwd + "'"           
             cursor.execute(query)
             rs = cursor.fetchall()            
 
+            # If Logged in...
             if len(rs) > 0:
                 user_id   = rs[0] # Gets user ID after login validation
                 user_name = str(rs[0][1]).strip() + ' ' + str(rs[0][2]).strip() # Gets First Name / Last Name after login validation
-                query = 'SELECT * FROM public."Results" WHERE person_id = ' + "'" + str(user_id[0]) + "'"           
-                cursor.execute(query)
-                rs = cursor.fetchall()
+                location  = rs[0][3]
+                results, dates = [None],['']
 
-                return render_template('main.html', data=rs, user=user_name)
+                query = 'SELECT weigh_in, weigh_in_dt FROM public."Results" WHERE person_id = ' + "'" + str(user_id[0]) + "'"           
+                cursor.execute(query)
+                rs = cursor.fetchall()               
+
+                for x in rs:
+                    #print(x[0], ' | and | ' , x[1] )
+                    results.append( float(x[0]) )
+                    dates.append(  str(x[1]) )
+                
+                results.append( None )
+                dates.append( '' )    
+
+                print(dates)
+                return render_template('main.html', data=results, labels=dates, user=user_name, location=location)
+            
+            # If Login Fails...
             else:
                 return render_template('login.html', error='Invalid user / Incorrect Password')           
 
